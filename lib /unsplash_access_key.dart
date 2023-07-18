f86b6ed4a9246d89a0b32aaf23df923e5d34c5f9
Meryem Özlem AYDOĞAN <82104183+meryemozlem@users.noreply.github.com>
2023-07-18 11:39:18 +0300

    Create unsplash_access_key.dart

---
+++ lib /unsplash_access_key.dart
@@ -0,0 +1,6 @@
+
+// TODO: Retrieve an API Access Key from https://unsplash.com/developers
+const String unsplashAccessKey = '';
+
+// The application name for the above API Access Key.
+const unsplashAppName = '';
